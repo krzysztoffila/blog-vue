a1f3b89f665bb46f1ff53459064dee554419dbb5
krzysztoffila <umowy.fila@gmail.com>
2024-07-24 14:22:50 +0200

    Style: Fix content structure in single blog

--- src/views/SingleBlog.vue
+++ src/views/SingleBlog.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="single-blog container">
-    <h1 class="text-center">{{ blog.title_pl }}</h1>
-    <h2 class="text-center">{{ blog.subtitle_pl }}</h2>
-    <img
-      class="img-fluid mx-auto d-block"
-      :src="blog.photo"
-      alt=""
-      style="max-width: 500px; max-height: 500px"
-    />
-    <p class="m-5">{{ blog.content_pl }}</p>
+  <div class="single-blog container d-flex flex-column align-items-center">
+    <h1 class="text-center mt-5 pb-2">{{ blog[`title_${lang}`] }}</h1>
+    <h2 class="text-center mb-5">{{ blog[`subtitle_${lang}`] }}</h2>
+    <img class="img-fluid mx-auto d-block mb-4" :src="blog.photo" alt="" />
+    <div class="blog-content mb-5">
+      <p v-html="formattedContent"></p>
+    </div>
   </div>
 </template>
 
@@ -20,8 +17,35 @@ export default {
         (blog) => blog.id == this.$route.params.blogId
       );
     },
+    lang() {
+      return this.$store.state.Lang.lang;
+    },
+    formattedContent() {
+      return this.blog[`content_${this.lang}`].replace(/\n/g, "<br />");
+    },
   },
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.single-blog {
+  max-width: 800px;
+  padding: 20px;
+}
+h1,
+h2 {
+  color: #333;
+}
+.blog-content {
+  line-height: 1.6;
+  font-size: 1.1em;
+  color: #555;
+  white-space: pre-line;
+}
+img {
+  max-width: 100%;
+  height: auto;
+  max-height: 500px;
+  object-fit: cover;
+}
+</style>
